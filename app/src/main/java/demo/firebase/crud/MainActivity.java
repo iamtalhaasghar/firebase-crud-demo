c1231ac98914e0fc545e7b77896cf34ca296e67c
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2021-03-14 11:29:52 +0500

    init

---
+++ app/src/main/java/demo/firebase/crud/MainActivity.java
@@ -0,0 +1,24 @@
+package demo.firebase.crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference();
+        DatabaseReference child = ref.child("Child Name");
+        child.setValue("Child Value");
+
+    }
+}
